4d372d17b915bf8cd88323513904fa8a1d833bcd
praveenteju <134632562+praveenteju@users.noreply.github.com>
2023-08-15 12:35:05 -0400

    first commit
    first commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+   //
+      // implement node event listeners here
+      baseUrl:'https://www.southwest.com/'
+  //  },
+  },
+});
